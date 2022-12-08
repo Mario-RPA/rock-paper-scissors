e8f28c5a16a5de98aea7a3172eb39d0df305230a
Mario Orquera <mario.orquera.p@gmail.com>
2022-12-08 20:45:01 -0300

    Add round function, changed computer choice to return a number only

--- rps.js
+++ rps.js
@@ -1,6 +1,28 @@
 function getComputerChoice() {
-    let choices = ["Rock", "Paper","Scissors"];
-    return choices[Math.floor(Math.random()*3)];
+    return Math.floor(Math.random()*3);
 }
 
-window.onload = getComputerChoice();
+function round() {
+    let choices = ["rock", "paper","scissors"];    
+    let computerChoice = choices[getComputerChoice()];
+    let userChoice = prompt("Enter your choice 'rock', 'paper' or 'scissors'");
+    if (userChoice === null || userChoice === "" || (!choices.includes(userChoice.toLowerCase()))) {
+        round();
+    } else if (userChoice === computerChoice) {
+        return "It's a tie!";
+    } else if (userChoice === "scissors" && computerChoice === "rock") {
+        return false;
+    } else if (userChoice === "scissors" && computerChoice === "paper") {
+        return true;
+    } else if (userChoice === "paper" && computerChoice === "rock") {
+        return true;
+    } else if (userChoice === "paper" && computerChoice === "scissors") {
+        return false;
+    } else if (userChoice === "rock" && computerChoice === "paper") {
+        return false;
+    } else if (userChoice === "rock" && computerChoice === "scissors") {
+        return true;
+    }
+}
+
+window.onload = round();
