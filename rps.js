91682162cb5dbdcc3c337aa0adc68f1b3aa76dd6
Mario Orquera <mario.orquera.p@gmail.com>
2022-12-09 22:15:13 -0300

    Remove commented old code

--- rps.js
+++ rps.js
@@ -19,23 +19,6 @@ function scoreboard(userChoice, computerChoice, userScore, computerScore) {
 function playRound(message = "Enter your choice 'rock', 'paper' or 'scissors'") {
     let computerChoice = getComputerChoice();
     let userChoice = prompt(message);
-    // if (userChoice === null || userChoice === "" || (!choices.includes(userChoice.toLowerCase()))) {
-    //     return playRound("Error: either canceled, empty or wrong selection, try again:");
-    // } else if (userChoice === computerChoice) {
-    //     return playRound("Tie! Try again!");
-    // } else if (userChoice === "scissors" && computerChoice === "rock") {
-    //     return [false, userChoice, computerChoice];
-    // } else if (userChoice === "scissors" && computerChoice === "paper") {
-    //     return [true, userChoice, computerChoice];
-    // } else if (userChoice === "paper" && computerChoice === "rock") {
-    //     return [true, userChoice, computerChoice];
-    // } else if (userChoice === "paper" && computerChoice === "scissors") {
-    //     return [false, userChoice, computerChoice];
-    // } else if (userChoice === "rock" && computerChoice === "paper") {
-    //     return [false, userChoice, computerChoice];
-    // } else if (userChoice === "rock" && computerChoice === "scissors") {
-    //     return [true, userChoice, computerChoice];
-    // }
     if (userChoice === null || userChoice === "" || (!choices.includes(userChoice.toLowerCase()))) {
         return playRound("Error: either canceled, empty or wrong selection, try again:");
     } else if (choices.indexOf(userChoice) === computerChoice) {
