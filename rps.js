4760909416e32dd3203200347a1d21f34a20448d
Mario Orquera <mario.orquera.p@gmail.com>
2022-12-09 21:57:26 -0300

    Replace playRound logic to evaluating a matrix instead of a bunch of ifs, add some other functions to support it

--- rps.js
+++ rps.js
@@ -1,27 +1,31 @@
+const choices = ["rock", "paper","scissors"]; 
+const matches = [
+    [0, 2, 1],
+    [1, 0, 2],
+    [2, 1, 0],
+];
 function getComputerChoice() {
     return Math.floor(Math.random()*3);
 }
 
+function choiceTranslator(choice) {
+    return choices[choice];
+}
+
+function scoreboard(userChoice, computerChoice, userScore, computerScore) {
+    return `\n\nUser ${userScore} - ${computerScore} Computer\n\nUser chose: ${userChoice}\nComputer chose: ${computerChoice}`;
+}
+
 function playRound(message = "Enter your choice 'rock', 'paper' or 'scissors'") {
-    let choices = ["rock", "paper","scissors"];    
-    let computerChoice = choices[getComputerChoice()];
+    let computerChoice = getComputerChoice();
     let userChoice = prompt(message);
     if (userChoice === null || userChoice === "" || (!choices.includes(userChoice.toLowerCase()))) {
         return playRound("Error: either canceled, empty or wrong selection, try again:");
-    } else if (userChoice === computerChoice) {
+    } else if (choices.indexOf(userChoice) === computerChoice) {
         return playRound("Tie! Try again!");
-    } else if (userChoice === "scissors" && computerChoice === "rock") {
-        return [false, userChoice, computerChoice];
-    } else if (userChoice === "scissors" && computerChoice === "paper") {
-        return [true, userChoice, computerChoice];
-    } else if (userChoice === "paper" && computerChoice === "rock") {
-        return [true, userChoice, computerChoice];
-    } else if (userChoice === "paper" && computerChoice === "scissors") {
-        return [false, userChoice, computerChoice];
-    } else if (userChoice === "rock" && computerChoice === "paper") {
-        return [false, userChoice, computerChoice];
-    } else if (userChoice === "rock" && computerChoice === "scissors") {
-        return [true, userChoice, computerChoice];
+    } else {
+        userChoice = choices.indexOf(userChoice);
+        return [matches[userChoice][computerChoice], userChoice, computerChoice];
     }
 }
 
@@ -29,12 +33,14 @@ function game(rounds = 5) {
     let userScore = 0, computerScore = 0;
     for (let i = 0; i < rounds; i++) {
         let roundResults = playRound();
-        if (roundResults[0]) {
+        let userChoice = choiceTranslator(roundResults[1]);
+        let computerChoice = choiceTranslator(roundResults[2]);
+        if (roundResults[0] === 1) {
             userScore++;
-            alert(`You won the round!\n\nUser ${userScore} - ${computerScore} Computer\n\nUser chose: ${roundResults[1]}\nComputer chose: ${roundResults[2]}`);            
-        } else {
+            alert(`You won the round! ${scoreboard(userChoice, computerChoice, userScore, computerScore)}`);
+        } else if (roundResults[0] === 2) {
             computerScore++;
-            alert(`You lost the round!\n\nUser ${userScore} - ${computerScore} Computer\n\nUser chose: ${roundResults[1]}\nComputer chose: ${roundResults[2]}`);
+            alert(`You lost the round! ${scoreboard(userChoice, computerChoice, userScore, computerScore)}`);
         }
     }
     if (userScore === computerScore) {
